afa3ac461850c42d76ade0f51c64e0b7362e9003
Ray <chenxiaolei.ray@gmail.com>
2017-02-04 19:05:43 +0800

    完成显示当前用户的列表

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 /**
  * Created by Ray on 2017/2/4.
  */
-var app = require("express")();
+var express = require("express");
+var path = require("path");
 
-app.listen(3000);
+var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello world\n');
+app.use(express.static(path.join(__dirname, 'public')));
+
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+server.listen(3000);
+
+var userList = [];
+
+io.on('connection', function (socket) {
+    socket.emit('userList', userList);
+
+    socket.on("name",function (data) {
+        userList.push({
+            name:data.name,
+            id:socket.id
+        });
+        console.log(userList);
+    })
+
 });
